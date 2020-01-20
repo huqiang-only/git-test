a7b89c8aef1628ab4906f9db2b727139eac92ee6
huqiang-only <1203145987@qq.com>
2020-01-20 19:35:53 +0800

    gengxing

--- testDemo.kt
+++ testDemo.kt
@@ -2,4 +2,7 @@ fun main() {
     println(numberZoo.maxBy{it.age})
     println("maxBy height is info : ${numberZoo.maxBy{it.height}}")
     println("minBy age is info ：${numberZoo.minBy{it.age}}")
+    println(numberZoo.filter{
+        (it.age > 18) and (it.height > 168) and (it.age < 25)
+    })
 }
